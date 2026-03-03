e175128c171052a651058e5f23802d75a6fbd991
tonevvinie <tonegwengwe@gmail.com>
2026-03-03 13:28:46 +0200

    add EntryView placeholder

---
+++ apps/web/app/entry/[entryId]/page.tsx
@@ -0,0 +1,12 @@
+import SaveButton from "../../../components/SaveButton";
+
+export default function EntryPage() {
+  return (
+    <div className="p-4">
+      <div className="bg-gray-200 h-64 rounded-lg mb-4 flex items-center justify-center">
+        Video Player Placeholder
+      </div>
+      <SaveButton />
+    </div>
+  );
+}
